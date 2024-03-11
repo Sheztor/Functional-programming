811c92c1578de93d9e23cd58da4a5d7f6b922493
Sheztor <hfsa2327@gmail.com>
2024-03-11 15:16:13 -0400

    README.md added and comments for first-class-functions

---
+++ README.md
@@ -0,0 +1,14 @@
+# :gear: Functional Programming - JavaScript
+
+## First-class functions
+
+In JavaScript, functions are first-class, which means they can be assigned to variables, passed as arguments to other functions, and returned as values from other functions.
+
+#### Code example
+
+```JS
+const sum = (a, b) => a + b;
+
+const result = sum(2, 3); // Using the function like any other value
+console.log(result); // Output: 5
+```
--- first-class-functions.js
+++ first-class-functions.js
@@ -1,17 +1,17 @@
-
+// Function assigned to constant
 const sum = (x, y) => x + y;
 
 console.log(sum(5, 5))
 
-
+// Function declared at runtime
+// Function being passed as an argument to another function
 setTimeout(() => console.log("Hi"), 2000)
 
-
+// A function returning another function
 function sayHi(saludo){
   return function name(name){
     return saludo + " " + name
   }
 }
-
 const sayHiUser = sayHi("Hi")
 console.log(sayHiUser("Héctor"))
