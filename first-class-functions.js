54d73bcbb325a8be59c96ecb33fce2396d88a5f8
Sheztor <hfsa2327@gmail.com>
2024-03-11 14:59:30 -0400

    first-class-functions - Function returning a function

--- first-class-functions.js
+++ first-class-functions.js
@@ -5,3 +5,13 @@ console.log(sum(5, 5))
 
 
 setTimeout(() => console.log("Hi"), 2000)
+
+
+function sayHi(saludo){
+  return function name(name){
+    return saludo + " " + name
+  }
+}
+
+const sayHiUser = sayHi("Hi")
+console.log(sayHiUser("Héctor"))
